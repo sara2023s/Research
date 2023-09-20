483f679ce470d203f3a5d379d278e3e4ae3e9e23
sara2023s <145309696+sara2023s@users.noreply.github.com>
2023-09-20 13:17:42 +1200

    Add files via upload

---
+++ helpdesk-2.py
@@ -0,0 +1,56 @@
+from ticket import TicketSystem
+
+def main():
+    print("Welcome to the Help Desk Ticketing System Prototype")
+    
+    while True:
+        print("\nOptions:")
+        print("1. Create a Ticket")
+        print("2. Resolve a Ticket")
+        print("3. Reopen a Ticket")
+        print("4. Provide Response to a Ticket")
+        print("5. Display Ticket Statistics")
+        print("6. Exit")
+        
+        choice = input("Enter your choice (1/2/3/4/5/6): ")
+        
+        if choice == '1':
+            staff_id = input("Enter Staff ID: ")
+            creator_name = input("Enter Ticket Creator Name: ")
+            contact_email = input("Enter Contact Email: ")
+            description = input("Enter Description of the Issue: ")
+            TicketSystem.submit_ticket(staff_id, creator_name, contact_email, description)
+            print("The ticket has been created successfully")
+        
+        elif choice == '2':
+            ticket_number = int(input("Enter Ticket Number to Resolve: "))
+            TicketSystem.resolve_ticket(ticket_number)
+            print(f"Ticket {ticket_number} resolved.")
+        
+        elif choice == '3':
+            ticket_number = int(input("Enter Ticket Number to Reopen: "))
+            TicketSystem.reopen_ticket(ticket_number)
+            print(f"Ticket {ticket_number} reopened.")
+        
+        elif choice == '4':
+            ticket_number = int(input("Enter Ticket Number to Provide Response: "))
+            response = input("Enter Response: ")
+            TicketSystem.provide_response(ticket_number, response)
+            print(f"Response added to Ticket {ticket_number}.")
+        
+        elif choice == '5':
+            created, resolved, open = TicketSystem.get_ticket_statistics()
+            print(f"Tickets Created: {created}")
+            print(f"Tickets Resolved: {resolved}")
+            print(f"Tickets To Solve: {open}")
+        
+        elif choice == '6':
+            print("Exiting the Help Desk Ticketing System.")
+            break
+        
+        else:
+            print("Invalid choice. Please select a valid option (1/2/3/4/5/6).")
+
+if __name__ == "__main__":
+    main()
+
