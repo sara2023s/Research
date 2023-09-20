781ee5a45daa1d8129c9053b854ba0f0cfdcccc9
sara2023s <145309696+sara2023s@users.noreply.github.com>
2023-09-20 13:24:15 +1200

    Update helpdesk-2.py

--- helpdesk-2.py
+++ helpdesk-2.py
@@ -4,9 +4,11 @@
 
 from ticket import TicketSystem
 
+ 
 def main():
     print("Welcome to the Help Desk Ticketing System Prototype")
-    
+
+#This first part is what allows users to choose what they will do. This function prints the choices so they can pick one. 
     while True:
         print("\nOptions:")
         print("1. Create a Ticket")
@@ -17,7 +19,8 @@ def main():
         print("6. Exit")
         
         choice = input("Enter your choice (1/2/3/4/5/6): ")
-        
+
+#If the user has chosen to create a ticket it requires additional information such as, creator name, a contact email, description and staff ID.
         if choice == '1':
             staff_id = input("Enter Staff ID: ")
             creator_name = input("Enter Ticket Creator Name: ")
@@ -25,29 +28,34 @@ def main():
             description = input("Enter Description of the Issue: ")
             TicketSystem.submit_ticket(staff_id, creator_name, contact_email, description)
             print("The ticket has been created successfully")
-        
+
+#The resolve ticket choice requires the ticket number you want to resolve
         elif choice == '2':
             ticket_number = int(input("Enter Ticket Number to Resolve: "))
             TicketSystem.resolve_ticket(ticket_number)
             print(f"Ticket {ticket_number} resolved.")
-        
+
+#To reopen a ticket it also requires the ticket number 
         elif choice == '3':
             ticket_number = int(input("Enter Ticket Number to Reopen: "))
             TicketSystem.reopen_ticket(ticket_number)
             print(f"Ticket {ticket_number} reopened.")
-        
+
+#In order to respont to a ticket it again needs the ticket number and the your response.
         elif choice == '4':
             ticket_number = int(input("Enter Ticket Number to Provide Response: "))
             response = input("Enter Response: ")
             TicketSystem.provide_response(ticket_number, response)
             print(f"Response added to Ticket {ticket_number}.")
-        
+
+#This choice displays the ticket information such as, the number of created, resolved and open tickets. 
         elif choice == '5':
             created, resolved, open = TicketSystem.get_ticket_statistics()
             print(f"Tickets Created: {created}")
             print(f"Tickets Resolved: {resolved}")
             print(f"Tickets To Solve: {open}")
-        
+
+#And lastly if youre all done using the software this will exit out of it. 
         elif choice == '6':
             print("Exiting the Help Desk Ticketing System.")
             break
